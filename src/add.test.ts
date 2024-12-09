33def2857fea9730accf79683eb5fd72db37a4a0
Hadrien Thomas <hadrien.thomas@zenika.com>
2024-12-09 22:56:08 +0100

    [chore] mutation testing

--- src/add.test.ts
+++ src/add.test.ts
@@ -3,7 +3,6 @@ import { add } from './add';
 
 describe('add', () => {
   test('2 and 3 makes 5', () => {
-    console.log("vla le log", add);
     const actualResult = add(2, 3);
     expect(actualResult).toBe(5);
   });
